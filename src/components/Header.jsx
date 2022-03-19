a4ab1737f715976a6d7756c22a6a9139257f017c
1lucascq <1lucascq@gmail.com>
2022-03-19 17:55:14 -0300

    setup: testando deployment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,25 +11,25 @@ import Typography from '@mui/material/Typography';
 import { CssBaseline, Paper } from '@mui/material';
 import PlanetsContext from '../context/PlanetsContext';
 import UseSwitchesCustom from './DarkThemeSwitch';
-import StarWarsFont from '../assets/fonts/starwarsfont.ttf';
+// import StarWarsFont from '../assets/fonts/starwarsfont.ttf';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'StarWarsFont, Roboto',
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
-        @font-face {
-          font-family: 'StarWarsFont';
-          font-style: normal;
-          font-display: swap;
-          font-weight: 400;
-          src: "local('StarWarsFont'), local('StarWarsFont-Regular'), url(${StarWarsFont}) format('ttf')";
-          unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
-        }
-      `,
-    } } });
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: 'StarWarsFont, Roboto',
+//   },
+//   components: {
+//     MuiCssBaseline: {
+//       styleOverrides: `
+//         @font-face {
+//           font-family: 'StarWarsFont';
+//           font-style: normal;
+//           font-display: swap;
+//           font-weight: 400;
+//           src: "local('StarWarsFont'), local('StarWarsFont-Regular'), url(${StarWarsFont}) format('ttf')";
+//           unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
+//         }
+//       `,
+//     } } });
 
 export default function Header() {
   const { darkMode, setDarkMode } = useContext(PlanetsContext);
@@ -47,16 +47,16 @@ export default function Header() {
             >
               <MenuIcon />
             </IconButton> */}
-            <ThemeProvider theme={ theme }>
-              <CssBaseline />
-              <Typography
-                variant="h6"
-                component="div"
-                sx={ { flexGrow: 1, fontFamily: 'StarWarsFont' } }
-              >
-                StarWars Planets API
-              </Typography>
-            </ThemeProvider>
+            {/* <ThemeProvider theme={ theme }>
+              <CssBaseline /> */}
+            <Typography
+              variant="h6"
+              component="div"
+              sx={ { flexGrow: 1, fontFamily: 'StarWarsFont' } }
+            >
+              StarWars Planets API
+            </Typography>
+            {/* </ThemeProvider> */}
 
             <UseSwitchesCustom />
             {/* <Switch
